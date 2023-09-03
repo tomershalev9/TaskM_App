59e31e824f7e9f5d6e8171888e2cdeecbd22c19e
xDarkN <nadavedri1372@gmail.com>
2023-09-03 12:08:24 +0300

    js update

--- static/js/script.js
+++ static/js/script.js
@@ -375,7 +375,6 @@ function updateDisplay(){
                     <p>${element.category}</p>
                 </div>
                 <div class="event-content ${divMarginColor}">
-                    <img src="assets/menu-card.png" class="menu-card m-${dayButtonSelected}">
                     <h3>${element.name}</h3>
                     <p>${element.comment}</p>
                 </div>
